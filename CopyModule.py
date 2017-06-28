31f7eddc10c326b7ab17f30260d18194b1a39bc4
Leon Wang <piscesleon@gmail.com>
2017-06-29 00:06:53 +0800

    添加对字典的操作演示

--- CopyModule.py
+++ CopyModule.py
@@ -4,3 +4,10 @@ cheese=copy.copy(spam)
 cheese[1]=42
 print(spam)
 print(cheese)
+
+
+# 在字典中添加 键-值
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+print(birthdays)
+birthdays['Additional']='Added Date'
+print(birthdays)
