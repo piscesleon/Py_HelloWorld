1ed6f2a76ad841a5c2540f7bebc7468fa7090ef8
Leon Wang <piscesleon@gmail.com>
2017-06-30 23:55:58 +0800

    添加一个测试临时代码用的文件

---
+++ Test.py
@@ -0,0 +1,21 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+
+text = pyperclip.paste()
+
+# Separate lines and add stars.
+lines = text.split('\n')
+
+print(lines)
+
+print(len(lines))
+
+for i in range(len(lines)): # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+print(lines)
+print(text)
+pyperclip.copy(text)
