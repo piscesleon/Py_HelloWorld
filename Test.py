d70984b79520a80768c6857ec967c99cd32743c3
Leon Wang <piscesleon@gmail.com>
2017-07-01 00:12:49 +0800

    规则化所有PY的文件头

--- Test.py
+++ Test.py
@@ -2,7 +2,8 @@
 # bulletPointAdder.py - Adds Wikipedia bullet points to the start
 # of each line of text on the clipboard.
 
-import pyperclip
+import pyperclip # 导入剪贴板
+import re # 导入正则表达式
 
 text = pyperclip.paste()
 
