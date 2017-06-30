6ad1402654920080abe11b4d9bace6abe443731f
Leon Wang <piscesleon@gmail.com>
2017-07-01 01:02:23 +0800

    regular update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Py_HelloWorld
+# Python笔记
+
--- Test.py
+++ Test.py
@@ -5,18 +5,9 @@
 import pyperclip # 导入剪贴板
 import re # 导入正则表达式
 
-text = pyperclip.paste()
 
-# Separate lines and add stars.
-lines = text.split('\n')
+# 以下放置测试代码
 
-print(lines)
-
-print(len(lines))
-
-for i in range(len(lines)): # loop through all indexes in the "lines" list
-    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
-text = '\n'.join(lines)
-print(lines)
-print(text)
-pyperclip.copy(text)
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242., 111-222-3322')
+print('Phone number found: ' + mo.group())
