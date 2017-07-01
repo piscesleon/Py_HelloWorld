ae9b4c3ce42d2ee30e1f341e6226091cba0403e4
Leon Wang <piscesleon@gmail.com>
2017-07-01 14:46:38 +0800

    添加email识别正则表达式

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -16,6 +16,14 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
     )''', re.VERBOSE)
 
-# TODO: Create email regex.
+# Create email regex.
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @ # @ symbol
+    [a-zA-Z0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot-something
+    )''', re.VERBOSE)
+
 # TODO: Find matches in clipboard text.
 # TODO: Copy results to the clipboard.
