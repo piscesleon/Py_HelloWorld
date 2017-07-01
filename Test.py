610d6ee33e2c0ce9838145374548ce044c5c798e
Leon Wang <piscesleon@gmail.com>
2017-07-01 14:40:49 +0800

    格式化PY文件头

--- Test.py
+++ Test.py
@@ -1,12 +1,12 @@
 #! python3
-# bulletPointAdder.py - Adds Wikipedia bullet points to the start
-# of each line of text on the clipboard.
+# 这里放程序说明
 
 import pyperclip # 导入剪贴板
 import re # 导入正则表达式
 
+# TODO: 这里放置所有待办事项
 
-# 以下放置测试代码
+# 以下放置代码
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4242., 111-222-3322')
