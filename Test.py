f1ba1f88e57f2be0b3ae022e9366dc2a5495aa16
Leon Wang <piscesleon@gmail.com>
2017-07-02 17:51:30 +0800

    update

--- Test.py
+++ Test.py
@@ -5,6 +5,7 @@ import os
 import re # 导入正则表达式
 import pyperclip # 导入剪贴板
 import shelve # 导入用于保存变更的二进制文件模块
+import requests #另外安装的模块，用于下载网页内容
 import logging #导入日志
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -13,12 +14,10 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 # TODO: 这里放置所有待办事项
 
 # 以下放置代码
-
-
-for folderName, subfolders, filenames in os.walk('D:\\Project'): # 遍历所有文件夹、及子文件夹，并用for列出所有父子文件夹与文件名
-    print('The current folder is ' + folderName)
-    for subfolder in subfolders:
-        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
-    for filename in filenames:
-        print('FILE INSIDE ' + folderName + ': '+ filename)
-    print('')
+os.chdir('D:\\Project\\Python')
+res = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000): #每10万字节写入一次
+    playFile.write(chunk)
+playFile.close()
--- mapIt.py
+++ mapIt.py
@@ -5,6 +5,7 @@ import webbrowser
 import sys
 import re # 导入正则表达式
 
+
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:]) # 返回的列表中，第1个值为py程序名，因此以 [1:] 规避第1个参数
@@ -13,4 +14,3 @@ print(address)
 
 webbrowser.open_new_tab('https://www.baidu.com/s?wd='+address)
 
-# TODO: Get address from clipboard.
